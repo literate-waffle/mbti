49620560169c29a5c0bc76885e7ce8cb815e553c
florimondmanca <florimond.manca@gmail.com>
2019-03-04 15:57:40 +0100

    refactory tweepy API factory

--- twitter/hello.py
+++ twitter/hello.py
@@ -4,21 +4,7 @@ From:
 https://tweepy.readthedocs.io/en/3.7.0/getting_started.html
 """
 
-import os
-import tweepy
-import dotenv
-
-dotenv.load_dotenv()
-
-
-def create_api():
-    auth = tweepy.OAuthHandler(
-        os.getenv("TWITTER_CONSUMER_KEY"), os.getenv("TWITTER_CONSUMER_SECRET")
-    )
-    auth.set_access_token(
-        os.getenv("TWITTER_ACCESS_TOKEN"), os.getenv("TWITTER_TOKEN_SECRET")
-    )
-    return tweepy.API(auth)
+from .api import create_api
 
 
 def main():
